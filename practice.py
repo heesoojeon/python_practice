478ad5b0f7400de5fc644f35bd8b6da323b9f7fd
heesoojeon <a01023648978@gmail.com>
2023-02-06 22:58:26 +0900

    w

---
+++ practice.py
@@ -0,0 +1,31 @@
+str1 = '720'
+print(type(str1))
+to_int = int(str1)
+print(to_int)
+print(type(to_int))
+
+num = 100
+print(num, type(num))
+to_str = str(num)
+print(to_str, type(to_str))
+
+str2 = "15.79"
+float_num = float(str2)
+
+print(float_num, type(float_num))
+
+letters = 'python'
+
+print(letters[0],'',letters[2])
+
+licence_plate = '24가 2210'
+print(licence_plate[-4:])
+
+name1 = '김민수'
+age1 = 10
+name2 = '이철희'
+age2 = 13
+
+print('이름: %s 나이: %d'%(name1,age1))
+print("이름: %s 나이: %d"%(name2,age2))
+
